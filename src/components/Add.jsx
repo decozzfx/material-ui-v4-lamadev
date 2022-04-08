d00d0936c155efa7a61ffd575fd99c15bf8d762d
decozzfx <m.f.r.ozil@gmail.com>
2022-04-08 14:26:13 +0700

    build modal, alert, and fill right bar

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,8 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Container, Fab, FormControlLabel, FormLabel, makeStyles, MenuItem, Modal, Radio, RadioGroup, Snackbar, TextField, Tooltip } from '@material-ui/core'
+import MuiAlert from '@material-ui/lab/Alert'
+import { Add as AddIcon } from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) => ({
+    fab: {
+        position : 'fixed',
+        bottom : 30,
+        right : 30
+    },
+    container: {
+        width : 500,
+        height : 550,
+        backgroundColor : 'white',
+        position : 'absolute',
+        top : 0,
+        right : 0,
+        bottom : 0,
+        left: 0,
+        margin : 'auto',
+        [theme.breakpoints.down('xs')] : {
+            width : '100%',
+            height : '100vh',
+        }
+    },
+    form: {
+        padding : theme.spacing(2)
+    },
+    item: {
+        marginBottom : theme.spacing(3)
+    }
+}))
+
+function Alert(props){
+    return <MuiAlert elevation={6} variant='filled' {...props} />
+}
 
 const Add = () => {
+    const classes = useStyles()
+    const [ openModal , setOpenModal ] = useState(false)
+    const [ openAlert, setOpenAlert ] = useState(false)
+
+    function handleClose(event, reason){
+        if(reason === 'clickaway'){
+            return
+        }
+        setOpenAlert(false)
+    }
+
   return (
-    <div>Add</div>
+    <>
+        <Tooltip title='Add' aria-label='add' onClick={() => setOpenModal(true)} >
+            <Fab color='primary'  className={classes.fab}>
+                <AddIcon />
+            </Fab>    
+        </Tooltip>   
+        <Modal open={openModal}>
+            <Container className={classes.container}>
+                <form className={classes.form} autoComplete='off' >
+                    <div className={classes.item}>
+                        <TextField id='standart-basic' label='Title' size='small' fullWidth />
+                    </div>
+                    <div className={classes.item}>
+                        <TextField 
+                        id='outlined-multiline-static'
+                        multiline
+                        rows={4}
+                        variant='outlined'
+                        placeholder='Tell your story...'
+                        label='Description' 
+                        size='small' 
+                        fullWidth />
+                    </div>
+                    <div className={classes.item}>
+                        <TextField select label='Visibility' value='Public' style={{ width : '50%' }}>
+                            <MenuItem value='Public'>Public</MenuItem>
+                            <MenuItem value='Private'>Private</MenuItem>
+                            <MenuItem value='Unlisted'>Unlisted</MenuItem>
+                        </TextField>
+                    </div>
+                    <div className={classes.item}>
+                        <FormLabel component='legend'>Who can comment?</FormLabel>
+                            <RadioGroup>
+                                <FormControlLabel value='Everybody' control={<Radio size='small' />} label='Everybody' />
+                                <FormControlLabel value='My Friends' control={<Radio size='small' />} label='My Friends' />
+                                <FormControlLabel value='Only Me' control={<Radio size='small' />} label='Only Me' />
+                            </RadioGroup>
+                    </div>
+                    <div className={classes.item}>
+                        <Button variant='outlined' color='primary' style={{ marginRight : 20 }} onClick={() => setOpenAlert(true, setOpenModal(false))} >Create</Button>
+                        <Button variant='outlined' color='secondary' onClick={() => setOpenModal(false)} >Cancel</Button>
+                    </div>
+                </form>
+            </Container>
+        </Modal>
+        <Snackbar open={openAlert} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical : 'bottom', horizontal : 'left' }}>
+            <Alert>
+                Your story has been posted successfully
+            </Alert>
+        </Snackbar>
+    </>
   )
 }
 
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
-import { Container, makeStyles, Typography } from '@material-ui/core'
+import React from "react";
+import { Avatar, Container, makeStyles, Typography } from "@material-ui/core";
+import { AvatarGroup } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    paddingTop : theme.spacing(2)
-  }
-}))
+    paddingTop: theme.spacing(2),
+  },
+}));
 
 const Rightbar = () => {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <Container className={classes.container}>
-      rightbar
+      <Typography>Online Friends</Typography>
+      <AvatarGroup max={5}>
+         { friends.map(friend => (
+         <Avatar alt={friend.alt} src={friend.src} key={friend.alt} />
+         )) }
+      </AvatarGroup>
     </Container>
-  )
-}
+  );
+};
 
-export default Rightbar
+const friends = [
+   {
+      alt : 'remi',
+      src : 'https://material-ui.com/static/images/avatar/1.jpg'
+   },
+   {
+      alt : 'travis',
+      src : 'https://material-ui.com/static/images/avatar/2.jpg'
+   },
+   {
+      alt : 'cindy',
+      src : 'https://material-ui.com/static/images/avatar/3.jpg'
+   },
+   {
+      alt : 'agnes',
+      src : 'https://material-ui.com/static/images/avatar/4.jpg'
+   },
+   {
+      alt : 'trevor',
+      src : 'https://material-ui.com/static/images/avatar/5.jpg'
+   },
+]
+
+export default Rightbar;
